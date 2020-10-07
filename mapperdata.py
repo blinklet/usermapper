077c3f8bbf40f512314380b63f791f54363d0761
Brian Linkletter <mail@brianlinkletter.ca>
2020-10-06 21:37:14 -0400

    Replaced mapperdata.py code with new. Added random password function.

--- mapperdata.py
+++ mapperdata.py
@@ -1,77 +1,27 @@
 #!/usr/bin/env python3
-from copy import copy, deepcopy
+import yaml, secrets, string
 
-sshparams = {
-    "protocol" : "ssh",
-    "hostname" : "",
-    "port" : "22",
-    "username" : "root",
-    "password" : "root",
-    "color-scheme" : "black-white",
-    "enable-sftp" : "true",
-    "sftp-root-directory" : "/"
-}
+def get_user_passwords(pass_value,length=8):
+    if pass_value == "random":
+        alphabet = string.ascii_letters + string.digits
+        passwd = ''.join(secrets.choice(alphabet) for i in range(length))
+    else:
+        passwd = pass_value
+    return passwd
 
-r1params = copy(sshparams)
-r2params = copy(sshparams)
-r3params = copy(sshparams)
-r4params = copy(sshparams)
-r5params = copy(sshparams)
-
-devices = {
-    "Router01" : r1params,
-    "Router02" : r2params,
-    "Server01" : r3params,
-    "Server02" : r4params,
-    "Server03" : r5params
-}
-
-s1device = deepcopy(devices)
-s2device = deepcopy(devices)
-s3device = deepcopy(devices)
-s4device = deepcopy(devices)
-
-
-
-student01 = dict(
-    username="student01",
-    password="lI62d4WUDKNv",
-    devices=s1device
-)
-
-student02 = dict(
-    username="student02",
-    password="UwBF9HJ3Astu",
-    devices=s2device
-)
-
-student03 = dict(
-    username="student03",
-    password="passwortdddd",
-    devices=s3device
-)
-
-student04 = dict(
-    username="student04",
-    password="ppasswword",
-    devices=s4device
-)
-
-students = [student01, student02, student03]
-
-ip = {
-    "Router01" : "10.0.40.1",
-    "Router02" : "10.0.40.2",
-    "Server01" : "10.0.40.101",
-    "Server02" : "10.0.40.102",
-    "Server03" : "10.0.40.103"
-}
-            
 
 if __name__ == "__main__":
-    print(students)
-    print()
-    print(ip)
-    print()
+
+    stream = open('config.yaml', 'r')
+    configuration = yaml.safe_load(stream)
+
+    for x in configuration['users']:
+        print(x)
+        for y in range(configuration['users'][x]["quantity"]):
+            z = configuration['users'][x]["password"]
+            print(z)
+            print(get_user_passwords(z))
+            print()
+    
     
 
