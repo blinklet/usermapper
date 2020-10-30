ccc5d4072b2a0c6c15f463f63fca32b8510b8d19
Brian Linkletter <mail@brianlinkletter.ca>
2020-10-30 12:04:40 -0400

    Added package name to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+    name='UserMapper',
     url='https://github.com/blinklet/usermapper',
     author='Brian Linkletter',
     author_email='mail@brianlinkletter.ca',
@@ -8,6 +9,6 @@ setup(
     install_requires=['PyYAML'],
     version='0.1',
     license='GPLv3',
-    description='An example of a python package from pre-existing code',
-    long_description=open('README.md').read(),
+    description='Create a user-mappimg.xml file',
+    long_description=open('README.md').read()
 )
